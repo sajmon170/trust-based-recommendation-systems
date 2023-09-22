d0cdd90b3831cfa295c965b7473f0b2504af50e8
Paras Stefanopoulos <parasstefans@hotmail.com>
2023-09-22 18:55:52 +1000

    working

--- Code/JaccardRandomWalkMC.py
+++ Code/JaccardRandomWalkMC.py
@@ -1,51 +1,55 @@
-from collections import deque
+
 import random
 import numpy as np
 from utils import *
 from common import *
+from collections import deque
 
-def jacc_random_walk_mc(graph, ratings, rated_by, item, test_points):
-    rb = set(rated_by[item])
-    recommendations = {n : ratings[n][item] for n in rb}
-    jacc_graph = jacc_graph = create_jacc_connected_graph(graph, JACCARD_MATRIX)
-    
+
+def jacc_random_walk_mc(data, item, test_points):
+    rb = set(data['rated_by'][item])
+    recommendations = {n: data['ratings'][n][item] for n in rb}
+
     def _out_voters(v):
-        return len(set(graph[v]).intersection(set(recommendations.keys())))
-    
+        return len(set(data['trust_graph'][v]).intersection(set(recommendations.keys())))
+
     def _monte_carlo(node):
         def _traverse(n, d=0, alpha=0.05):
             if n in recommendations:
                 return recommendations[n]
-            
+
             # With alpha * d probability skip to a random node
             if random.random() < d * alpha:
                 return _traverse(random.choice(list(recommendations.keys())))
-            
-            next_nodes = [o_n for o_n, _ in jacc_graph[n]]
-            softmax = lambda x: np.exp(x)/sum(np.exp(x))
-            next_jaccs = softmax([jacc for _, jacc in jacc_graph[n]])
+
+            next_nodes = list(set(data['trust_graph'][n]) - {n})
+            def softmax(x): return np.exp(x)/sum(np.exp(x))
+
+            next_jaccs = softmax(
+                list([data['jaccard_idx'][n][j] for j in next_nodes]))
             if len(next_nodes) == 0:
                 return None
 
             return _traverse(np.random.choice(next_nodes, p=next_jaccs), d + 1)
-        
+
         results = []
         for _ in range(100):
             cr = _traverse(node)
             if cr is not None:
                 results.append(cr)
-        
+
         if len(results) == 0:
             return None
         return np.mean(results)
-        
-    q = deque(sorted(list(test_points), key = lambda x: _out_voters(x), reverse=True))
-    
+
+    q = deque(sorted(list(test_points),
+              key=lambda x: _out_voters(x), reverse=True))
+
     while (len(q)):
         current = q.popleft()
         res = _monte_carlo(current)
         if res is None:
             res = random.randint(1, 5)
         recommendations[current] = res
-        
+
     return recommendations
--- Code/RandomWalkMC.py
+++ Code/RandomWalkMC.py
@@ -2,48 +2,48 @@ from collections import deque
 import random
 import numpy as np
 from utils import *
-from common import JACCARD_MATRIX
 
-def random_walk_mc(graph, ratings, rated_by, item, test_points):
-    rb = set(rated_by[item])
-    recommendations = {n : ratings[n][item] for n in rb}
-    jacc_graph = create_jacc_connected_graph(graph, JACCARD_MATRIX)
-    
+
+def random_walk_mc(data, item, test_points):
+    rb = set(data['rated_by'][item])
+    recommendations = {n: data['ratings'][n][item] for n in rb}
+
     def _out_voters(v):
-        return len(set(graph[v]).intersection(set(recommendations.keys())))
-    
+        return len(set(data['trust_graph'][v]).intersection(set(recommendations.keys())))
+
     def _monte_carlo(node):
         def _traverse(n, d=0, alpha=0.05):
             if n in recommendations:
                 return recommendations[n]
-            
+
             # With alpha * d probability skip to a random node
             if random.random() < d * alpha:
                 return _traverse(random.choice(list(recommendations.keys())))
-            
-            next = graph[n]
+
+            next = data['trust_graph'][n]
             if len(next) == 0:
                 return None
 
             return _traverse(random.choice(next), d + 1)
-        
+
         results = []
         for _ in range(100):
             cr = _traverse(node)
             if cr is not None:
                 results.append(cr)
-        
+
         if len(results) == 0:
             return None
         return np.mean(results)
-        
-    q = deque(sorted(list(test_points), key = lambda x: _out_voters(x), reverse=True))
-    
+
+    q = deque(sorted(list(test_points),
+              key=lambda x: _out_voters(x), reverse=True))
+
     while (len(q)):
         current = q.popleft()
         res = _monte_carlo(current)
         if res is None:
             res = random.randint(1, 5)
         recommendations[current] = res
-        
+
     return recommendations
